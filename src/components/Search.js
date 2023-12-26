2147942df74ddc6e193dbd7fdf9c829d95054ef6
Ayush Singh <135166710+ayush-2405@users.noreply.github.com>
2023-12-27 00:28:30 +0530

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,7 @@ import { MdSearch } from 'react-icons/md';
 
 const Search = ({ handleSearchNote }) => {
 	return (
-		<div className='search'>
+		<div className='search head'>
 			<MdSearch className='search-icons' size='1.3em' />
 			<input
 				onChange={(event) =>
